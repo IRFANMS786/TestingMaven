e7f813171fb807a32b691c41cd1220d5b2d66a15
IRFANMS786 <67092834+IRFANMS786@users.noreply.github.com>
2024-07-23 21:07:55 +0530

    New

---
+++ src/main/java/programmingProblems/Calculator.java
@@ -0,0 +1,37 @@
+package programmingProblems;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner s1 = new Scanner(System.in);
+		int a, b;
+		System.out.println("Enter the number to be Calculate...");
+		a = s1.nextInt();
+		b = s1.nextInt();
+		System.out.println("Choose operator to perform operations +,-,*,/,%");
+		char ch;
+		ch = s1.next().charAt(0);
+		switch (ch) {
+		case '+':
+			System.out.println("The Addition of two Number.." + (a + b));
+			break;
+		case '-':
+			System.out.println("The Subraction of two Number.." + (a - b));
+			break;
+		case '*':
+			System.out.println("The Multiplication of two Number.." + (a * b));
+			break;
+		case '/':
+			System.out.println("The Divison of two Number.." + (a / b));
+			break;
+		case '%':
+			System.out.println("The Divison of two Number.." + (a % b));
+			break;
+		default:
+			System.out.println("Invalid opertion");
+		}
+
+	}
+
+}
