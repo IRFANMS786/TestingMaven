c769c0ec3e718a32693234780658a03621075e69
IRFANMS786 <67092834+IRFANMS786@users.noreply.github.com>
2024-07-25 14:04:34 +0530

    day18
    day18

---
+++ src/main/java/programmingProblems/FactorialNumber.java
@@ -0,0 +1,18 @@
+package programmingProblems;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		int number, fact = 1;
+		System.out.println("Enter the Count to be perform Factortial operation ");
+		Scanner s1 = new Scanner(System.in);
+		number = s1.nextInt();
+		for (int i = 1; i <= number; i++) {
+			fact = fact * i;
+		}
+		System.out.println("The Factorial of a number is..." + fact);
+
+	}
+
+}
--- src/main/java/programmingProblems/Reverseastring.java
+++ src/main/java/programmingProblems/Reverseastring.java
@@ -6,6 +6,7 @@ public class Reverseastring {
 
 	public static void main(String[] args) {
 		Scanner s1 = new Scanner(System.in);
+		System.out.println("The Enter the  String To check  Palindrome String or Not ..");
 		String originalStr = s1.nextLine();
 //		String originalStr = "ollo";
 		String reversedStr = "";
@@ -14,7 +15,7 @@ public class Reverseastring {
 			reversedStr = originalStr.charAt(i) + reversedStr;
 			System.out.println(reversedStr);
 		}
-
+		System.out.println("Orignial string: " + originalStr);
 		System.out.println("Reversed string: " + reversedStr);
 		if (originalStr.compareTo(reversedStr) == 0) {
 			System.out.println("The Given String is Palindrome String..");
